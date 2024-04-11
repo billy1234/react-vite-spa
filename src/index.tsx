10d7c5bffae5fd9cf3ece4a3c84cea1bcb4449b8
Billy Mihalarias <bmihalarias@gmail.com>
2024-04-11 13:28:48 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  UuiBaseTheme,
-  UuiDefaultTheme,
-  UuiGlobalVariablesTheme,
-  UuiInverseTheme,
-} from "@aonunited/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <UuiDefaultTheme />
-    <UuiInverseTheme />
-    <UuiGlobalVariablesTheme />
-    <UuiBaseTheme />
     <App />
   </React.StrictMode>,
 );
